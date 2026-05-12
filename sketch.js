494ad8061f412674289b73ac46d7bfe22d31bbbc
Anshera S <anshera.siddique@avantika.edu.in>
2026-05-12 22:27:03 +0530

    Initial p5.js setup

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 function setup() {
+<<<<<<< HEAD
   createCanvas(800, 600);
 }
 
@@ -7,4 +8,14 @@ function draw() {
 
   textSize(32);
   text("Hello Visual Novel", 200, 300);
+=======
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background(220);
+
+    textSize(32);
+    text("Hello Visual Novel", 200, 300);
+>>>>>>> e1e805c (Initial p5.js setup)
 }
