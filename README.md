03cb877945aa41e3b95f5fb24723bdbe675a11fb
anshikapandya-rgb <anshika.pandya@avantika.edu.in>
2026-05-12 22:23:49 +0530

    initial p5.js setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Visual Novel Project
+
+A visual novel game made using p5.js.
---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(220);
+
+  textSize(32);
+  text("Hello Visual Novel", 200, 300);
+}
