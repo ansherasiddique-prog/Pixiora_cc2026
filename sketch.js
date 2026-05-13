ce016b764194b9d6a26b8655d3f57ad7287201f7
Anshera S <anshera.siddique@avantika.edu.in>
2026-05-14 00:08:45 +0530

    Update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
-let currentScene=1;
-let dIndex = 0;
+/* ---------------- GLOBAL STATE ---------------- */
+let gameState = "START";
+let dialogueIndex = 0;
+
 let stickers = [];
-let activeSticker = "⭐";
-let options = ["❤️", "⭐", "🌸"];
-let introText = ["Mother's!", "No money...", "I'll make a card!", "Let's go!"];
-let finalText = ["Happy Mothers day!", "I made this.", "I love it!"];
+let selectedSticker = "🌸";
+
+let stickerOptions = ["❤️", "⭐", "🌸", "🍀"];
+
+let dialogues = [
+    "Today is mamma's birthday but I don't have money to buy her a gift...",
+    "I'll make her a greeting card instead!",
+    "Mama always smiles when I draw for her.",
+    "Let's go to my room and make something special!"
+];
+
+let finalDialogues = [
+    "Happy Birthday Mom! I made this for you.",
+    "Sweetie... this is beautiful. I love it so much!"
+];
