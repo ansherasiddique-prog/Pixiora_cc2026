a63058bc5b66d1a10d25409c35361017743ec926
Anshera S <anshera.siddique@avantika.edu.in>
2026-05-13 11:01:55 +0530

    Fixed some issues with the structure.

--- sketch.js
+++ sketch.js
@@ -1,26 +1,11 @@
 let currentScene = 1;
 
 function setup() {
-<<<<<<< HEAD
-  createCanvas(800, 600);
-}
-
-function draw() {
-  background(220);
-
-  textSize(32);
-  text("Hello Visual Novel", 200, 300);
-=======
     createCanvas(800, 600);
 }
 
 function draw() {
 
-<<<<<<< HEAD
-    textSize(32);
-    text("Hello Visual Novel", 200, 300);
->>>>>>> e1e805c (Initial p5.js setup)
-=======
     if (currentScene === 1) {
         scene1();
     }
@@ -44,5 +29,4 @@ function draw() {
     else if (currentScene === 6) {
         scene6();
     }
->>>>>>> b80ddc0 (Added scene structure-took help from AI- Prompted:'Tell me what changes to i make to my index and sketch files to combine these scenes sort of.')
 }
