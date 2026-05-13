27a5b22c47dabc1a5395831d8e5a4002e2baf1a3
Anshera S <anshera.siddique@avantika.edu.in>
2026-05-13 22:02:34 +0530

    start of code

--- sketch.js
+++ sketch.js
@@ -1,32 +1,6 @@
-let currentScene = 1;
+let currentScene=1;
+let dIndex = 0;
+let stickers = [];
+let activeSticker = "⭐";
+let options = ["❤️", "⭐", "🌸"];
 
-function setup() {
-    createCanvas(800, 600);
-}
-
-function draw() {
-
-    if (currentScene === 1) {
-        scene1();
-    }
-
-    else if (currentScene === 2) {
-        scene2();
-    }
-
-    else if (currentScene === 3) {
-        scene3();
-    }
-
-    else if (currentScene === 4) {
-        scene4();
-    }
-
-    else if (currentScene === 5) {
-        scene5();
-    }
-
-    else if (currentScene === 6) {
-        scene6();
-    }
-}
