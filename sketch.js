1e4b5358e461e811a363a21a66a3790f726925ec
Anshera S <anshera.siddique@avantika.edu.in>
2026-05-13 22:05:55 +0530

    Update

--- sketch.js
+++ sketch.js
@@ -3,4 +3,5 @@ let dIndex = 0;
 let stickers = [];
 let activeSticker = "⭐";
 let options = ["❤️", "⭐", "🌸"];
-
+let introText = ["Mother's!", "No money...", "I'll make a card!", "Let's go!"];
+let finalText = ["Happy Mothers day!", "I made this.", "I love it!"];
